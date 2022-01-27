060e184e287df18d7f03ce02ca96a15b6e979d07
mdsajedul <sajedulislms@gmail.com>
2022-01-28 02:33:49 +0600

    screenshot added

--- src/components/Project-Detail/ProjectDetail.js
+++ src/components/Project-Detail/ProjectDetail.js
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import "./projectDetail.css"
 
 const ProjectDetail = () => {
     const {projectId} = useParams();
     const [project,setProject] = useState([]);
+    // const [rerender, setRerender] = useState(false);
+    const [imageOnView,setImageOnView]=useState('')
+
     useEffect(()=>{
         fetch(`/projects.json`)
         .then(res=>res.json())
         .then(data=>{
             const result = data.find( ({ id }) => id === parseInt(projectId) );
             setProject(result);
+            setImageOnView(result.imgmain);
         })
-    },[])
+    },[projectId])
+
+
+
+    const imageChange=(imageUrl)=>{
+        setImageOnView(imageUrl)
+        console.log(imageOnView)
+    }
+
     return (
         <div className='container'>
             <div className='row gx-0'>
@@ -19,7 +33,18 @@ const ProjectDetail = () => {
                     <h3>Project Preview {projectId}</h3>
                     <div className='left-container'>
                         <div className='image-container-project'>
-                            <img src={project.imgmain} className='container-fluid' alt="" />
+                            <img src={imageOnView} className='container-fluid' alt="" />
+                        </div>
+                        <div className='small-container-img d-flex'>
+                            {
+                                project?.images?.map(image=> <div className='mx-2'>
+                                    <button onClick={()=>imageChange(image)}>
+                                        <img className='small-img-view' src={image} alt="" />
+                                    </button>
+                                    
+                                </div>)
+                                
+                            }
                         </div>
                     </div>
                 </div>
